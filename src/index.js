04d564b89adf81b6c95492bf24f95c7966a01c36
fogelkuba <fogelkuba@gmail.com>
2019-05-15 19:32:16 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res, err) => {
     // res.send('<h1> Hello World! </h1>')
     res.sendFile(__dirname + '/index.html')
-})
+});
+
+io.on('connection', () => {
+    console.log('a user connected');
+});
 
 http.listen(3000, () => {
     console.log('Listening on port *:3000')
